6e0ebe40ce4388e07bc9de718440e5f0490055d1
蘇健豪 <jenhausu@icloud.com>
2019-12-09 17:37:55 +0800

    Initial Commit

---
+++ SleepTracker-Swift/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SleepTracker-Swift
+//
+//  Created by 蘇健豪 on 2019/12/9.
+//  Copyright © 2019 蘇健豪. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
