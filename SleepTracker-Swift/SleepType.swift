3e3b9fdf6fc62735a3845fe38981611e6031f022
蘇健豪 <jenhausu@icloud.com>
2019-12-20 16:57:25 +0800

    create SleepType Enum

---
+++ SleepTracker-Swift/SleepType.swift
@@ -0,0 +1,14 @@
+//
+//  SleepType.swift
+//  SleepTracker-Swift
+//
+//  Created by 蘇健豪 on 2019/12/20.
+//  Copyright © 2019 蘇健豪. All rights reserved.
+//
+
+import Foundation
+
+enum SleepType {
+    case normal
+    case nap
+}
