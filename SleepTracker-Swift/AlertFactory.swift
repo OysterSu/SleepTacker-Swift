0510a49da88abb3e33a1ee2908fdf05a830ecf0d
蘇健豪 <jenhausu@icloud.com>
2019-12-25 15:03:39 +0800

    AlertFactory init

---
+++ SleepTracker-Swift/AlertFactory.swift
@@ -0,0 +1,21 @@
+//
+//  AlertFactory.swift
+//  SleepTracker-Swift
+//
+//  Created by 蘇健豪1 on 2019/12/25.
+//  Copyright © 2019 蘇健豪. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class AlertFactory {
+    
+    class func errorAlert(_ error: Error) -> UIAlertController {
+        let alert = UIAlertController(title: NSLocalizedString("錯誤", comment: ""), message: error.localizedDescription, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: NSLocalizedString("確定", comment: ""), style: .default, handler: nil)
+        alert.addAction(confirm)
+        
+        return alert
+    }
+}
