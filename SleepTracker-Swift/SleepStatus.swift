0b850f01e16cbec73e7511e71deb3520b99a38d3
蘇健豪 <jenhausu@icloud.com>
2019-12-25 21:23:45 +0800

    create SleepStatus singleton

---
+++ SleepTracker-Swift/SleepStatus.swift
@@ -0,0 +1,16 @@
+//
+//  SleepStatus.swift
+//  SleepTracker-Swift
+//
+//  Created by 蘇健豪1 on 2019/12/25.
+//  Copyright © 2019 蘇健豪. All rights reserved.
+//
+
+import Foundation
+
+class SleepStatus {
+    
+    static let shared = SleepStatus()
+    
+    private init() {}
+}
