244b9ce5c57f9e6d6c1db86dc8fbfaf0bd723e03
蘇健豪 <jenhausu@icloud.com>
2019-12-25 21:09:30 +0800

    success alert

--- SleepTracker-Swift/AlertFactory.swift
+++ SleepTracker-Swift/AlertFactory.swift
@@ -11,6 +11,14 @@ import UIKit
 
 class AlertFactory {
     
+    class func successAlert(title: String) -> UIAlertController {
+        let alert = UIAlertController(title: NSLocalizedString(title, comment: ""), message: nil, preferredStyle: .alert)
+        let confirm = UIAlertAction(title: NSLocalizedString("確定", comment: ""), style: .default, handler: nil)
+        alert.addAction(confirm)
+        
+        return alert
+    }
+    
     class func errorAlert(_ error: Error) -> UIAlertController {
         let alert = UIAlertController(title: NSLocalizedString("錯誤", comment: ""), message: error.localizedDescription, preferredStyle: .alert)
         let confirm = UIAlertAction(title: NSLocalizedString("確定", comment: ""), style: .default, handler: nil)
